9d2e81d3f6834baced39159dbb5c40dd3472ef82
LYG49 <98518027+LYG49@users.noreply.github.com>
2023-02-18 17:04:41 +0800

    Update socket_image.py

--- socket_image.py
+++ socket_image.py
@@ -1,31 +1,23 @@
+#####################################################導入函式庫######################################################
+
 import socket
+
+###################################################建立socket連線####################################################
 
 host = '120.117.40.244'
 port = 8082
 address = (host,port)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(address)
 
+######################################################資料傳輸#######################################################
 
-socket02 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-socket02.connect(address)
-
-
-
-###########################################################################
-
-#開始傳輸
 print('start send image')
 imgFile = open("2_7.jpg", "rb")
-
 while True:
     imgData = imgFile.readline(1024)
-
     if not imgData:
         break
-    socket02.send(imgData)
+    s.send(imgData)
 imgFile.close()
-print('transmit end')
-###########################################################################
-
-socket02.close()
-print('client close')
+s.close()
