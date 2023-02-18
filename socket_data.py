dfa2503645578600090ab31a59fd81802652dd96
LYG49 <98518027+LYG49@users.noreply.github.com>
2023-02-18 17:01:26 +0800

    Update socket_data.py

--- socket_data.py
+++ socket_data.py
@@ -1,17 +1,22 @@
+#####################################################導入函式庫######################################################
+
 import socket
 
-
+###################################################建立socket連線####################################################
 
 host = '192.168.137.23'
 port = 3306
 address = (host,port)
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        #定義IPV4、TCP協定
-s.connect(address)                                           #取得IP,PORT
-with open ("C:/Users/user/yolov7/result.txt" ,"r") as f:                                 #開啟txt檔案
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                 #定義IPV4、TCP協定
+s.connect(address)                                                    #取得IP,PORT
+
+######################################################資料傳輸#######################################################
+
+with open ("C:/Users/user/yolov7/result.txt" ,"r") as f:              #開啟txt檔案
     content = f.read()                                                #讀取
     readlines = content.split(":")                                    #用:做為斷點
     for line in readlines:                                            #將資料用for迴圈撈出來
         print(line) 
-text_data = line                                                    #將取得的值存入text_data
-s.sendall(text_data.encode())                                #傳輸
+text_data = line                                                      #將取得的值存入text_data
+s.sendall(text_data.encode())                                         #傳輸
 s.close() 
