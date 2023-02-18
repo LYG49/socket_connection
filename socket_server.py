1fb843d82bc72301861b445c5282eb35ca07d6cf
LYG49 <98518027+LYG49@users.noreply.github.com>
2023-02-18 17:09:00 +0800

    Update socket_server.py

--- socket_server.py
+++ socket_server.py
@@ -1,14 +1,20 @@
+#####################################################導入函式庫######################################################
+
 import socket
 
-ip_address = '0.0.0.0'
-port = 8585
+###################################################建立socket連線####################################################
+
+host = '0.0.0.0'
+port = 3306
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((ip_address, port))
+server_socket.bind((host, port))
 server_socket.listen(5)
+
+####################################################接收文字訊息#####################################################
 
 while True:
     client_socket, client_address = server_socket.accept()
     print("Accepted connection from: ", client_address)
     data = client_socket.recv(1024).decode()
-    print("Needle: ", data)
+    print("content: ", data)
     client_socket.close()
